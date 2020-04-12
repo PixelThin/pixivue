ef4861ddd34caec5b7bd755096af467076d094e2
Dave Beck <ajqajq@gmail.com>
2020-04-12 12:14:24 -0500

    Fixes an issue with importing components into the default export

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import * as Transitions from './utilities/transitions'
-import pCountrySelect from 'components/p-country-select.vue'
-import pError from 'components/p-error.vue'
-import pSection from 'components/p-section.vue'
-import pSplit from 'components/p-split.vue'
-import pTranslucentBackground from 'components/p-translucent-background.vue'
+import pCountrySelect from './components/p-country-select.vue'
+import pError from './components/p-error.vue'
+import pSection from './components/p-section.vue'
+import pSplit from './components/p-split.vue'
+import pTranslucentBackground from './components/p-translucent-background.vue'
 export { Transitions, pCountrySelect, pError, pSection, pSplit, pTranslucentBackground }
 export default {
     install(Vue) {
